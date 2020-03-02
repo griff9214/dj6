d493221eecbda9346d392a6b6669c647c977c1f1
griff9214 <griff@list.ru>
2020-03-02 17:50:08 +0300

    let's make mmenu+hamburger

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,25 @@
 $(function() {
 
-	// Custom JS
+    $("header").before("<div id='my-menu'>");
+    $(".header-nav__list").clone().appendTo("#my-menu");
+    $("#my-menu").find("*").attr("style", "");
+    $("#my-menu").find("ul").removeClass();
 
+    const menu = new Mmenu( "#my-menu",{
+        extensions : [ 'pagedim-black'],
+    } );
+    const api = menu.API;
+    api.bind("close:start", function () {
+        $(".hamburger").removeClass("is-active");
+    });
+
+    api.bind("open:start", function () {
+        $(".hamburger").addClass("is-active");
+    });
+
+
+    $('.hamburger').click(function (event) {
+        api.open( $("#my-menu") );
+        event.preventDefault();
+    });
 });
-
-
-document.addEventListener(
-    "DOMContentLoaded", () => {
-        // Fire the plugin
-        const menu = new Mmenu( "#my-menu" );
-
-        // Get the API
-        const api = menu.API;
-
-        // Invoke a method
-        const panel = document.querySelector( "#my-panel" );
-        api.openPanel( panel );
-    }
-);
-
